d13fddf0ebff8f1a91d380089294fc9fd34d21b8
dpatel7 <61416876+dpatel7@users.noreply.github.com>
2020-09-11 17:34:16 +0100

    Updated Sh Script

--- Docker/script.sh
+++ Docker/script.sh
@@ -2,13 +2,10 @@
 
 DIR='/home/ubuntu/icap/input/'
 ODIR='/home/ubuntu/icap/output'
-
 rm -rf ${ODIR} > /dev/null 2>&1
-mkdir -m777 ${ODIR}
-
+mkdir -p ${ODIR}
 for file in "$DIR"*.pdf
 do
     filename=${file##*/}
-	sudo /usr/local/c-icap/bin/c-icap-client -i gw-icap01.westeurope.azurecontainer.io -p 1344 -s gw_rebuild -f "$file" -o "$ODIR"/"$filename" -v
-
+	   /usr/bin/c-icap-client -i gw-icap01.westeurope.azurecontainer.io -p 1344 -s gw_rebuild -f "$file" -o "$ODIR"/"$filename" -v
 done
