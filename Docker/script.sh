42b63415e235b421192340cb41c25010c7efa835
dpatel7 <61416876+dpatel7@users.noreply.github.com>
2020-09-17 13:58:06 +0100

    updated jmx and docker file

--- Docker/script.sh
+++ Docker/script.sh
@@ -4,7 +4,7 @@ DIR='/home/ubuntu/icap/input/'
 ODIR='/home/ubuntu/icap/output'
 rm -rf ${ODIR} > /dev/null 2>&1
 mkdir -p ${ODIR}
-for file in "$DIR"*.pdf
+for file in "$DIR"*.*
 do
     filename=${file##*/}
 	   /usr/bin/c-icap-client -i gw-icap01.westeurope.azurecontainer.io -p 1344 -s gw_rebuild -f "$file" -o "$ODIR"/"$filename" -v
